75fb4e9fa30f3b3a27f231dcacc293e8c1ca7c11
Tanya <tanya.sanjuan@gmail.com>
2025-04-29 00:20:28 +0100

    Introduction of the functions that the program will perform.

---
+++ analysis.py
@@ -0,0 +1,8 @@
+# This program analyzes the Iris dataset.
+# It loads the dataset, performs exploratory data analysis (EDA), and visualizes the data.
+# Outputs a summary of each variable to a single text file.
+# Saves a histogram of each variable to png files, and
+# Outputs a scatter plot of each pair of variables.
+# Author: Tanya San Juan.
+
+# Source to Iris dataset: https://archive.ics.uci.edu/dataset/53/iris
