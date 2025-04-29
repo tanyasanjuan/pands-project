9faaa05bdbb6dd4609f57dc906796291aeeae518
Tanya <tanya.sanjuan@gmail.com>
2025-04-29 23:44:17 +0100

    Export the libraries Pandas and Sklearn.
    Libraries that we need for the data analysis.

--- analysis.py
+++ analysis.py
@@ -5,4 +5,17 @@
 # Outputs a scatter plot of each pair of variables.
 # Author: Tanya San Juan.
 
-# Source to Iris dataset: https://archive.ics.uci.edu/dataset/53/iris
+# Import the libraries we need for data analysis.
+
+# Pandas is a data analysis library for Python.
+import pandas as pd
+
+# Scikit-learn is a machine learning library for Python. 
+# It contains datasets examples, including the Iris dataset.
+import sklearn as skl
+
+# Resources: https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+# To read the dataset, we use the read_csv function from the pandas library.
+# The dataset is in CSV format, and this specify the path to the file. 
+# Meaning it make the connection to a web server, to have a file.
+# Pandas helps to interpretate the file received.
